f8498112fd174e3367d24580083f05aa69bff687
suhyun113 <163711629+suhyun113@users.noreply.github.com>
2025-09-18 23:51:34 +0900

    feat: 고객 엔티티 수정

--- src/main/java/com/example/movie/domain/Customer.java
+++ src/main/java/com/example/movie/domain/Customer.java
@@ -1,9 +1,9 @@
-package com.example.movie.customer;
+package com.example.movie.domain;
 
 public class Customer {
     private final String userId;
     private final String userName;
-    private final int points;
+    private int points;
 
     public Customer(String userId, String userName, int points) {
         this.userId = userId;
@@ -14,4 +14,11 @@ public class Customer {
     public String getUserId() { return userId; }
     public String getUserName() { return userName; }
     public int getPoints() { return points; }
+
+    public void usePoints(int pointsToUse) {
+        if (this.points < pointsToUse) {
+            throw new IllegalArgumentException(pointsToUse + "로 보유 포인트가 부족합니다.");
+        }
+        this.points -= pointsToUse;
+    }
 }
