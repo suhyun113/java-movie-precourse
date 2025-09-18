3425c55e1aa75f6715fa81e15bd8046390c2fc9c
suhyun113 <163711629+suhyun113@users.noreply.github.com>
2025-09-19 00:01:58 +0900

    feat: PaymentType 할인율 추가

--- src/main/java/com/example/movie/enums/PaymentType.java
+++ src/main/java/com/example/movie/enums/PaymentType.java
@@ -1,6 +1,14 @@
 package com.example.movie.enums;
 
 public enum PaymentType {
-    CREDIT_CARD,
-    CASH
+    CREDIT_CARD(0.05),
+    CASH(0.02);
+
+    private final double discountRate;
+
+    PaymentType(double discountRate) {
+        this.discountRate = discountRate;
+    }
+
+    public double getDiscountRate() {return discountRate;}
 }
